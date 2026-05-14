f98301f10f41addfa0f9a2f79f302ec1b1e2c5b3
BaljeetAOL <bksaggu.kaur@gmail.com>
2026-05-14 15:54:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-alert("JS is working!");
-const images = [
-  "plant1.png",
-  "plant2.png",
-  "plant3.png",
-  "plant4.png"
-];
-console.log("JS loaded");
-let currentIndex = 0;
+function placeOrder() {
+    let total = 0;
+    let receipt = "";
 
-function openLightbox(index) {
-  currentIndex = index;
+    // SIZE
+    const size = document.querySelector('input[name="size"]:checked').value.split("|");
+    receipt += `<p>${size[0]}</p>`;
+    total += Number(size[1]);
 
-  document.getElementById("lightbox").style.display = "block";
-  document.getElementById("lightbox-img").src = images[currentIndex];
+    // CRUST
+    const crust = document.querySelector('input[name="crust"]:checked').value.split("|");
+    receipt += `<p>${crust[0]}</p>`;
+    total += Number(crust[1]);
+
+    // SAUCE
+    const sauce = document.querySelector('input[name="sauce"]:checked').value.split("|");
+    receipt += `<p>${sauce[0]}</p>`;
+
+    // CHEESE
+    const cheese = document.querySelector('input[name="cheese"]:checked').value.split("|");
+    receipt += `<p>${cheese[0]}</p>`;
+    total += Number(cheese[1]);
+
+    // VEGGIES
+    const vegs = document.querySelectorAll(".veg:checked");
+    vegs.forEach(v => receipt += `<p>${v.value}</p>`);
+    if (vegs.length > 1) total += vegs.length - 1;
+
+    // MEAT
+    const meats = document.querySelectorAll(".meat:checked");
+    meats.forEach(m => receipt += `<p>${m.value}</p>`);
+    if (meats.length > 1) total += meats.length - 1;
+
+    document.getElementById("orderList").innerHTML = receipt;
+    document.getElementById("total").innerHTML = `Total Price: $${total.toFixed(2)}`;
 }
-
-function closeLightbox() {
-  document.getElementById("lightbox").style.display = "none";
-}
-
-function changeSlide(step) {
-  currentIndex += step;
-
-  if (currentIndex >= images.length) currentIndex = 0;
-  if (currentIndex < 0) currentIndex = images.length - 1;
-
-  document.getElementById("lightbox-img").src = images[currentIndex];
-}
+``
